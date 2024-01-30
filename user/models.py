25f0b05f4e1b7d79bc302c100cc9f27bdce24dda
MuhammadRizo19 <muhammadrizosaidumarbekov19@gmail.com>
2024-01-30 17:35:07 +0500

    changes

--- user/models.py
+++ user/models.py
@@ -16,3 +16,17 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Point(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    player = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    points = models.IntegerField(default=0, blank=True, null=True)
+    added = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+       if self.player == Profile.objects.get(user=self.player):
+           self.player.points = self.player.points + self.points  
+       super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.player
